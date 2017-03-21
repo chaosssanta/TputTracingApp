cc5c2b7028b761f7822651d9e0fa03c1a125df26
heeyeon.nah <chaos_ssanta@daum.net>
2017-03-21 14:48:08 +0900

    implement exportToFile()

--- app/src/main/java/com/lge/tputtracingapp/data/DeviceStatsInfoStorageManager.java
+++ app/src/main/java/com/lge/tputtracingapp/data/DeviceStatsInfoStorageManager.java
@@ -1,10 +1,17 @@
 package com.lge.tputtracingapp.data;
 
+import android.os.Environment;
 import android.util.Log;
-
 import com.lge.tputtracingapp.service.DeviceLoggingStateChangedListener;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * Created by wonsik.lee on 2017-03-14.
@@ -12,14 +19,12 @@ import java.util.LinkedList;
 
 public class DeviceStatsInfoStorageManager implements DeviceLoggingStateChangedListener {
     private static final String TAG = DeviceStatsInfoStorageManager.class.getSimpleName();
-
     private LinkedList<DeviceStatsInfo> mDeviceStatsRecordList;
-
     private static DeviceStatsInfoStorageManager mInstance;
-
     private DeviceStatsInfoStorageManager() {
         this.mDeviceStatsRecordList = new LinkedList<>();
     }
+    private static final Executor mExecutor = Executors.newFixedThreadPool(1);
 
     public static DeviceStatsInfoStorageManager getInstance() {
         if (mInstance == null) {
@@ -28,16 +33,47 @@ public class DeviceStatsInfoStorageManager implements DeviceLoggingStateChangedL
         return mInstance;
     }
 
-    public void exportToFile(String fileName) {
+    public void exportToFile(final String fileName) {
         final LinkedList<DeviceStatsInfo> targetList = this.mDeviceStatsRecordList;
         this.mDeviceStatsRecordList = new LinkedList<>();
 
-        new Thread(new Runnable() {
+        Runnable run = new Runnable() {
             @Override
             public void run() {
-                // TODO : write to file here by chaossannta
+                handleFileWriting(targetList, fileName);
             }
-        }).start();
+        };
+        mExecutor.execute(run);
+    }
+
+    private void handleFileWriting(LinkedList<DeviceStatsInfo> targetList, String fileName) {
+        String dirPath = Environment.getExternalStorageDirectory().getPath();
+        byte[] buffer = null;
+        Iterator<DeviceStatsInfo> sIterator = targetList.iterator();
+        FileOutputStream fos = null;
+
+        File dir = new File(dirPath);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+
+        File file = new File(dir, fileName);
+        try {
+            fos = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            while (sIterator.hasNext()) {
+                buffer = sIterator.next().toString().getBytes();
+                fos.write(buffer, 0, buffer.length);
+                fos.flush();
+            }
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void add(DeviceStatsInfo deviceStatsInfo) {
