e30dc5c7055141ba1f9c088137e567896ed4ef7d
wonsik.lee <wonsiklee@gmail.com>
2017-06-09 14:50:58 +0900

    tput caculation modified. 2

--- app/src/main/java/com/lge/tputtracingapp/data/DeviceStatsInfoStorageManager.java
+++ app/src/main/java/com/lge/tputtracingapp/data/DeviceStatsInfoStorageManager.java
@@ -359,10 +359,10 @@ public class DeviceStatsInfoStorageManager implements DeviceMonitoringStateChang
     public float getCurrentTestAvgTput(TEST_TYPE type) {
         long targetBytes = (type == TEST_TYPE.DL_TEST) ? this.getCurrentTestTotalRxBytes(): this.getCurrentTestTotalTxBytes();
 
-        Log.d("WONSIK", "1024 : " + (targetBytes * 8.0f /1024/1024) / (this.getCurrentTestDurationTime(type) / 1000));
-        Log.d("WONSIK", "1000 : " + (targetBytes * 8.0f /1000/1000) / (this.getCurrentTestDurationTime(type) / 1000));
+        Log.d(TAG, "devided by 1024 : " + (targetBytes * 8.0f /1024/1024) / (this.getCurrentTestDurationTime(type) / 1000));
+        Log.d(TAG, "devided by 1000 : " + (targetBytes * 8.0f /1000/1000) / (this.getCurrentTestDurationTime(type) / 1000));
 
-        return (targetBytes * 8.0f /1024/1024) / (this.getCurrentTestDurationTime(type) / 1000.0f);
+        return (targetBytes * 8.0f /1000/1000) / (this.getCurrentTestDurationTime(type) / 1000.0f);
     }
 
     public long getCurrentTestDurationTime(TEST_TYPE type) {
